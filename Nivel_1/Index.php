c4843b7f8cbb4185a5ae70c6f1d1d20c6b32b0c6
Laura <amaxranthine1995@gmail.com>
2024-03-03 17:51:40 +0100

    creacion clase main

--- Nivel_1/Index.php
+++ Nivel_1/Index.php
@@ -3,3 +3,11 @@ Els animals tenen un nom, ara bé, no és el mateix el so de la “parla” d’
 Per tant, necessitem crear altres tipus de dades que ens ajudin a programar aquests comportaments. 
 Bàsicament, volem un mètode makeSound() 
 que mostri un missatge diferent si es tracta d’un gos (per exemple,“Bup, bup!”) o un gat (per exemple “Meu!”).*/
+<?php
+require_once "Animal.php";
+require_once "Gato.php";
+require_once "Perro.php";
+
+
+
+?>
